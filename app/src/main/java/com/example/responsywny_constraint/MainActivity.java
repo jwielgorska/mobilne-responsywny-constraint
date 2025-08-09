5a062d44dafa931f16b7fa0ba879ec83568c34cc
JW <j_wielgorska@wp.pl>
2025-08-09 23:24:18 +0200

    Initial commit - projekt mobilny (responsywny constraint)

---
+++ app/src/main/java/com/example/responsywny_constraint/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.responsywny_constraint;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Guideline;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+               textView = findViewById(R.id.textView);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float procenty = (float)(progress/100.0);
+                Guideline guideline = findViewById(R.id.guideline2);
+                guideline.setGuidelinePercent(procenty);
+                String str = "SeekBar  - wartość od 0 do 1.0  aktualna wartość \\n layout_constraintGuide_percent " +Float.toString(procenty);
+                textView.setText(str);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
